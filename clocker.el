eee3c75c1f65095ed73c7f1270ef5e26260e575f
Roman Gonzalez <roman@unbounce.com>
2015-03-25 11:10:48 -0700

    Bump to v0.0.7

--- clocker.el
+++ clocker.el
@@ -5,7 +5,7 @@
 
 ;; Author: Roman Gonzalez <romanandreg@gmail.com>
 ;; Maintainer: Roman Gonzalez <romanandreg@gmail.com>
-;; Version: 0.0.6
+;; Version: 0.0.7
 ;; Package-Requires: ((projectile "0.11.0") (dash "2.10"))
 ;; Keywords: org
 
