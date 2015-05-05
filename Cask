6faf5a23dc9cbe506f21a11fae4f6c4bbb1d4b01
Roman Gonzalez <roman@unbounce.com>
2015-05-05 08:42:20 -0700

    Bump to v0.0.10

--- Cask
+++ Cask
@@ -1,7 +1,7 @@
 (source gnu)
 (source melpa)
 
-(package "clocker" "0.0.9"
+(package "clocker" "0.0.10"
          "Helps to remember to clock-in on org files when coding")
 
 (package-file "clocker.el")
--- clocker.el
+++ clocker.el
@@ -5,7 +5,7 @@
 
 ;; Author: Roman Gonzalez <romanandreg@gmail.com>
 ;; Maintainer: Roman Gonzalez <romanandreg@gmail.com>
-;; Version: 0.0.9
+;; Version: 0.0.10
 ;; Package-Requires: ((projectile "0.11.0") (dash "2.10"))
 ;; Keywords: org
 
